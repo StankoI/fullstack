87353f5711c8c1d75be343a195715207b93b4acd
Stanislav <stanislavigov4@gmail.com>
2025-03-17 18:28:35 +0200

    finishing hw "remove images after 5 sec"

--- workplace/fmi-2025-03/github-promises.js
+++ workplace/fmi-2025-03/github-promises.js
@@ -1,34 +1,44 @@
+
 const resultsDiv = document.getElementById("results");
-
-function init(){
-    fetch("/users.json")
-    .then(userResp => {
-        return userResp.json();
-    }).then(users => {
-        console.log(users);
-        resultsDiv.innerHTML = users.reduce((acc,user) => 
-            acc + `<li>${user.username}</li>`
-        ,'')
-        return users.map(user => user.username);
-    }).then(usernames => 
-        Promise.allSettled( 
-            usernames.map(username => fetch(`https://api.github.com/users/${username}`)
-            .then(resp => resp.json())
-        )).then(gitresults =>{ 
-            const gitusers = gitresults
-            .filter(res => res.status === 'fulfilled')
-            .map(res=>res.value)
-            console.log(gitresults)
-            return gitresults    
+(function () {
+    fetch("users.json")
+        .then(usersResp => {
+            return usersResp.json()
+        }).then(users => {
+            // console.log(users)
+            // resultsDiv.innerHTML = users.reduce((acc, user) =>
+            //     acc + `<li>${user.username}</li>`
+            //     , '')
+            return users.map(user => user.username)
+        }).then(usernames =>
+            Promise.allSettled(
+                usernames.map(username =>
+                    fetch(`https://api.github.com/users/${username}`)
+                        .then(resp => resp.json())
+                )).then(gitresults => {
+                    const gitusers = gitresults
+                        .filter(res => res.status === 'fulfilled')
+                        .map(res => res.value)
+                    console.log(gitusers)
+                    return gitusers
+                })
+        ).then(gitusers => 
+            gitusers.map(gituser => {
+                const tmpImg = document.createElement("div");
+                tmpImg.classList.add("image");
+                tmpImg.style.backgroundImage = `url(${gituser.avatar_url})`;
+                resultsDiv.appendChild(tmpImg);
+                return tmpImg;
+            })
+        )
+        .then(images =>{
+            // TODO: remove images after 5 sec
+            setTimeout(
+                () => {
+                    images.forEach((img) => {img.style.display = "none"})
+                    console.log("images disappered")
+                }, 5000)
+            console.log(images)
         })
-    ).then(gitusers => {
-        gitusers.map(gituser => {
-            const img = new Image();
-            img.src = gituser.avatar_url;
-            resultsDiv.append(img)
-            return img
-        })
-    })
-}
-
-init();
+        // })
+})()
